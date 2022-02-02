05ed7d51f793b511b1a1bb822e5803e52ee17da6
GeeKocon <julek.kocon@gmail.com>
2022-02-02 16:32:29 +0100

    Forecast response edited

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,7 +4,6 @@ const request = require('request')
 const forecast = (latitude,longitude, callback) => {
 
     const url = 'http://api.weatherstack.com/current?access_key=8ae9c7ad6326e350fb8fc86762a63981&query=' + latitude + ',' + longitude + '&units=m';
-
     request({url, json: true}, (error, {body}) => {
         if(error){
             callback('No internet connection', undefined)
@@ -15,7 +14,7 @@ const forecast = (latitude,longitude, callback) => {
             const temperature = body.current.temperature;
             const windSpeed = body.current.wind_speed;
             const weatherDescription = body.current.weather_descriptions[0]
-            callback(undefined, 'It is currently ' + temperature + ' degrees out. There is a ' + windSpeed + ' wind speed. \n' + weatherDescription)
+            callback(undefined, 'It is currently ' + temperature + ' degrees out. There is a ' + windSpeed + ' wind speed.' + weatherDescription)
         }
     })
 }
